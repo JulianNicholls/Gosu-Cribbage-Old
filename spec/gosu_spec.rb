b95c5b7de38d45846f04e56b5726677e76c7a83d
Julian Nicholls <juliannicholls29@gmail.com>
2013-12-09 18:36:35 +0000

    Re-factored specs to use let/subject

--- spec/gosu_spec.rb
+++ spec/gosu_spec.rb
@@ -4,58 +4,60 @@ require 'spec_helper'
 
 
 describe Cribbage::GosuCard do
-  before :each do
-    @card = Cribbage::GosuCard.new( 1, 1 )
-  end
+  let( :card  ) { Cribbage::GosuCard.new( 1, 1 ) }
 
   it "should respond to set_display" do
-    expect( @card.respond_to? :set_display ).to eq true
+    expect( card.class.respond_to? :set_display ).to eq true
   end
 
-  it "should respond to draw" do
-    expect( @card.respond_to? :draw ).to eq true
-  end
+  subject { card }
+
+  it { should respond_to :draw }
+  it { should respond_to :x }
+  it { should respond_to :y }
+  it { should respond_to :x= }
+  it { should respond_to :y= }
 end
 
 
 describe Cribbage::GosuHand do
-  before :each do
-    pack  = Cribbage::Pack.new
-    @hand = Cribbage::GosuHand.new( pack, nil, nil, nil, nil )
-  end
+  let( :pack ) { Cribbage::Pack.new }
+  let( :hand ) { Cribbage::GosuHand.new pack }
+
+  subject { hand }
 
   it "should deal 6 Gosu cards" do
-    expect( @hand.cards.length ).to eq 6
-    expect( @hand.cards.all? { |c| c.is_a?( Cribbage::GosuCard ) } ).to eq true
+    expect( hand.cards.length ).to eq 6
+    expect( hand.cards.all? { |c| c.is_a?( Cribbage::GosuCard ) } ).to eq true
   end
 
   it "should deal cards in ascending rank/value order" do
-    5.times { |c| expect( @hand.cards[c].value <= @hand.cards[c+1].value ) }
-    5.times { |c| expect( @hand.cards[c].rank  <= @hand.cards[c+1].rank ) }
+    5.times { |c| expect( hand.cards[c].value <= hand.cards[c+1].value ) }
+    5.times { |c| expect( hand.cards[c].rank  <= hand.cards[c+1].rank ) }
   end
 
   it "should allow discarding of two cards" do
-    updated_hand = @hand.cards.dup
+    updated_hand = hand.cards.dup
     updated_hand.slice!( 1, 2 )   # 1 for 2
-    @hand.discard( *[1, 2] )       # 1 and 2
-    expect( @hand.cards.length ).to eq 4
-    expect( @hand.cards ).to eq updated_hand
+    hand.discard( *[1, 2] )       # 1 and 2
+    expect( hand.cards.length ).to eq 4
+    expect( hand.cards ).to eq updated_hand
 
     updated_hand.slice!( 1, 2 )   # 1 for 2
-    @hand.discard( 2, 1 )         # 1 and 2
-    expect( @hand.cards.length ).to eq 2
-    expect( @hand.cards ).to eq updated_hand
+    hand.discard( 2, 1 )          # 1 and 2
+    expect( hand.cards.length ).to eq 2
+    expect( hand.cards ).to eq updated_hand
   end
 
   it "should disallow discarding of more or less than two cards" do
-    expect { @hand.discard [1] }.to raise_error Exception
-    expect { @hand.discard [1, 2, 3] }.to raise_error Exception
+    expect { hand.discard [1] }.to raise_error Exception
+    expect { hand.discard [1, 2, 3] }.to raise_error Exception
   end
 
   it "should render the hand as text" do
-    @hand.cards = [FullPack::HA, FullPack::H5, FullPack::S5, FullPack::CJ, FullPack::HQ, FullPack::HK]
+    hand.cards = [FullPack::HA, FullPack::H5, FullPack::S5, FullPack::CJ, FullPack::HQ, FullPack::HK]
 
-    expect( @hand.to_s ).to eq 'AH 5H 5S JC QH KH'
+    expect( hand.to_s ).to eq 'AH 5H 5S JC QH KH'
   end
 end
 

c050e049c0baaba2afdd57616a80d8b005f65ef9
Julian Nicholls <juliannicholls29@gmail.com>
2013-12-10 20:07:12 +0000

    Adjusted button bottom colour

--- button.rb
+++ button.rb
@@ -7,6 +7,8 @@ class Button
   def initialize( window, text, font, colour, left, top, width = nil, height = nil )
     @window, @text, @font, @colour = window, text, font, colour
 
+    # If the width and/or height is not specified then measure the font and the button text
+
     width  ||= @font.text_width( text, 1 ) + 2 * @font.text_width( 'xx', 1 )
     height ||= @font.height * 2
 
@@ -14,10 +16,14 @@ class Button
   end
 
   def draw
+    # Centre the text on the button
+
     x_margin = (width - @font.text_width( @text, 1 )) / 2
     y_margin = (height - @font.height) / 2
 
-    lighter = Gosu::Color.new( 0xff, @colour.red * 2, @colour.green * 2, @colour.blue )
+    #  Colour for the bottom of the button
+
+    lighter = Gosu::Color.new( 0xff, @colour.red * 2, @colour.green * 2, @colour.blue * 2 )
 
     @window.draw_quad( left, top, @colour,
                        left + width, top, @colour,

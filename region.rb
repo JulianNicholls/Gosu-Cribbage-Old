c1b06a67bd8561dedf11895255ccdb209105b015
Julian Nicholls <juliannicholls29@gmail.com>
2013-12-10 14:36:00 +0000

    inside? now optionally takes an array with x and y

--- region.rb
+++ region.rb
@@ -14,9 +14,14 @@ module Region
     @top  += top_delta
   end
 
-  def inside?( x, y )
-    x.between?( @left, @left + @width ) &&
-    y.between?( @top, @top + @height )
+  def inside?( x, y = nil )
+    if x.is_a? Array
+      x[0].between?( @left, @left + @width ) &&
+      x[1].between?( @top, @top + @height )
+    else
+      x.between?( @left, @left + @width ) &&
+      y.between?( @top, @top + @height )
+    end
   end
 
 protected

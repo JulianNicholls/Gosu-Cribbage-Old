0a7463e209ce9a6eaf57f0be7e9e6e4d0d3555b8
Julian Nicholls <juliannicholls29@gmail.com>
2013-12-13 16:46:51 +0000

    Started on 31 play

--- cribbage.rb
+++ cribbage.rb
@@ -8,25 +8,29 @@ require './scorer'
 class CribbageGame < Gosu::Window
 
   DEAL        = 0
-  DISCARDING  = 1
-  CUT_CARD    = 2
-  PLAY_31     = 3
-  THE_SHOW    = 4
-  CRIB_SHOW   = 5
+  DISCARDING  = 10
+  CUT_CARD    = 20
+  PLAY_31     = 30
+  CPU_31      = 31
+  THE_SHOW    = 40
+  CRIB_SHOW   = 50
 
   WIDTH   = 800
   HEIGHT  = 600
 
-  MID_X   = WIDTH/2
-  MID_Y   = HEIGHT/2
+  MID_X   = WIDTH / 2
+  MID_Y   = HEIGHT / 2
 
-  BAIZE_COLOUR    = Gosu::Color.new( 0xff007000 )
-  DISCARD_COLOUR  = Gosu::Color.new( 0xff104ec2 )
+  BAIZE_COLOUR      = Gosu::Color.new( 0xff007000 )
+  SCORE_TEXT_COLOUR = Gosu::Color.new( 0xffffee00 )
+  SCORE_NUM_COLOUR  = Gosu::Color.new( 0xffffff00 )
+  WATERMARK_COLOUR  = Gosu::Color.new( 0x20000000 )
+  DISCARD_COLOUR    = Gosu::Color.new( 0xff104ec2 )
 
   CARD_HEIGHT = 150
   CARD_WIDTH  = 100
 
-  CARD_GAP    = 15
+  CARD_GAP    = 12
 
   COMPUTER_TOP  = CARD_GAP
   COMPUTER_LEFT = CARD_GAP
@@ -44,6 +48,12 @@ class CribbageGame < Gosu::Window
   DISCARD_TOP   = PLAYER_TOP - BUTTON_HEIGHT*2
   DISCARD_LEFT  = CARD_GAP*3
 
+  TOP_31  = COMPUTER_TOP + CARD_HEIGHT + CARD_GAP
+  LEFT_31 = CARD_GAP
+
+  SCORE_TOP  = CARD_GAP
+  SCORE_LEFT = WIDTH - CARD_WIDTH
+
   def initialize
     super( WIDTH, HEIGHT, false )
 
@@ -57,6 +67,8 @@ class CribbageGame < Gosu::Window
     @selected = []
     @game_phase = DISCARDING
     @show_crib = FALSE
+    @player_hand_31 = @cpu_hand_31 = nil
+    @player_score = @cpu_score = 100
   end
 
   def needs_cursor?   # Enable the mouse cursor
@@ -76,7 +88,8 @@ class CribbageGame < Gosu::Window
         @card_name = @card_cut.name  # DEBUG
         @score = Cribbage::Scorer.new( @player_hand, @card_cut ).evaluate # DEBUG
         @position = nil
-        # @game_phase = PLAY_31
+
+        setup_for_31
       end
 
     when DISCARDING
@@ -86,14 +99,19 @@ class CribbageGame < Gosu::Window
       else
         @position = nil if select_card
       end
+
+    when PLAY_31
+      # Blah!
+
+    when CPU_31
+      # Blah!
     end
   end
 
   def draw
     draw_background
-
-    @player_hand.draw :face_up
-    @computer_hand.draw :face_down
+    draw_hands
+    draw_scores
 
     # Always draw the spare pack, and then the cut card on top if it's set
 
@@ -103,6 +121,7 @@ class CribbageGame < Gosu::Window
     @discard_button.draw
     draw_crib if @show_crib
 
+    draw_31 if @game_phase.between? PLAY_31, CPU_31
     debug_display
   end
 
@@ -111,10 +130,40 @@ class CribbageGame < Gosu::Window
       0, 0, BAIZE_COLOUR, WIDTH-1, 0, BAIZE_COLOUR,
       WIDTH-1, HEIGHT-1, BAIZE_COLOUR, 0, HEIGHT-1, BAIZE_COLOUR, 0
     )
+
+    @font.draw( "Cribbage", 80, 230, 0, 1, 1, WATERMARK_COLOUR )
+  end
+
+  def draw_hands
+    if @game_phase.between? PLAY_31, CPU_31
+      unless @player_31_hand.nil?
+        @player_hand_31.draw :face_up
+        @cpu_hand_31.draw :peep     # :face_down
+      end
+    else
+      @player_hand.draw :face_up
+      @cpu_hand.draw :peep     # :face_down
+    end
+  end
+
+  def draw_scores
+    player = 'Player '
+    width  = @score_font.text_width( player, 1 );
+    height = @score_font.height
+
+    @score_font.draw( "CPU", SCORE_LEFT, SCORE_TOP, 1, 1, 1, SCORE_TEXT_COLOUR )
+    @score_font.draw( @cpu_score.to_s, SCORE_LEFT + width, SCORE_TOP, 1, 1, 1, SCORE_NUM_COLOUR )
+    @score_font.draw( player, SCORE_LEFT, SCORE_TOP + height, 1, 1, 1, SCORE_TEXT_COLOUR )
+    @score_font.draw( @player_score, SCORE_LEFT + width, SCORE_TOP + height, 1, 1, 1, SCORE_NUM_COLOUR )
   end
 
   def draw_crib
     @card_back_image.draw( CRIB_LEFT, CRIB_TOP, 1 )
+  end
+
+  def draw_31
+    @score_font.draw( 'Total', LEFT_31, TOP_31 - 20, 1, 1, 1, SCORE_TEXT_COLOUR )
+    @score_font.draw( @total_31, LEFT_31 + 50, TOP_31 - 20, 1, 1, 1, SCORE_NUM_COLOUR )
   end
 
   def button_down btn_id
@@ -131,7 +180,8 @@ class CribbageGame < Gosu::Window
   end
 
   def load_fonts
-    @font        = Gosu::Font.new( self, 'Arial', 50 )
+    @font        = Gosu::Font.new( self, 'Century Schoolbook L', 180 )
+    @score_font  = Gosu::Font.new( self, 'Serif', 20 )
     @card_font   = Gosu::Font.new( self, 'Arial', 28 )
     @button_font = Gosu::Font.new( self, 'Arial', 24 )
   end
@@ -143,8 +193,8 @@ class CribbageGame < Gosu::Window
     @pack.set_position( PACK_LEFT, PACK_TOP )
     @pack.set_images( @card_back_image, @card_front_image )
 
-    @player_hand   = Cribbage::GosuHand.new( @pack )
-    @computer_hand = Cribbage::GosuHand.new( @pack )
+    @player_hand = Cribbage::GosuHand.new( @pack )
+    @cpu_hand    = Cribbage::GosuHand.new( @pack )
 
     set_hand_positions
 
@@ -153,7 +203,7 @@ class CribbageGame < Gosu::Window
 
   def set_hand_positions
     @player_hand.set_positions( PLAYER_LEFT, PLAYER_TOP, CARD_WIDTH + CARD_GAP )
-    @computer_hand.set_positions( COMPUTER_LEFT, COMPUTER_TOP, CARD_WIDTH + CARD_GAP )
+    @cpu_hand.set_positions( COMPUTER_LEFT, COMPUTER_TOP, CARD_WIDTH + CARD_GAP )
   end
 
   def cut_card
@@ -185,7 +235,7 @@ class CribbageGame < Gosu::Window
 
   def discard_crib_cards
     @player_hand.discard( *@selected )
-    @computer_hand.discard( rand( 0..5 ), rand( 0..5 ) )
+    @cpu_hand.discard( rand( 0..5 ), rand( 0..5 ) )
 
     @selected = []
     @discard_button.hide()
@@ -195,16 +245,33 @@ class CribbageGame < Gosu::Window
     set_hand_positions
   end
 
+  def setup_for_31
+    @cpu_hand_31 = cpu_hand.dup
+    @player_hand31 = player_hand.dup
+    @run_cards = []
+    @run_num = -1
 
+    start_31_run
+
+    @game_phase = PLAY_31
+  end
+
+  def start_31_run
+    @total_31 = 0
+    @run_num += 1
+    @run_cards[@run_num] = []
+    @top_31   = TOP_31
+    @left_31  = LEFT_31 + (CARD_WIDTH+CARD_GAP) * @run_num
+  end
   def debug_display
-    dbg_str = ''
+    dbg_str = @game_phase.to_s
 
-    dbg_str += "Selected: #{@selected}" if !@selected.empty? || @game_phase == DISCARDING
+    dbg_str += " - Selected: #{@selected}" if !@selected.empty? || @game_phase == DISCARDING
     dbg_str += " - #{@position}"        if @position
     dbg_str += " - #{@card_name}"       if @card_name
     dbg_str += " - Score: #{@score}"    if @score
 
-    @button_font.draw( dbg_str, CARD_GAP, MID_Y, 1 ) unless dbg_str == ''
+    @button_font.draw( dbg_str, CARD_GAP, MID_Y, 10, 1, 1, 0x80000000 ) unless dbg_str == ''
   end
 end
 

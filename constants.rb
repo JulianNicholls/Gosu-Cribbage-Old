e05c6d14b72bb4212985088ce358052011b0ad48
Julian Nicholls <juliannicholls29@gmail.com>
2013-12-15 21:24:09 +0000

    Added comments

--- constants.rb
+++ constants.rb
@@ -1,5 +1,8 @@
 module CribbageGame
   module Constants
+
+    # Phases
+
     DEAL        = 0
     DISCARDING  = 10
     CUT_CARD    = 20
@@ -7,11 +10,15 @@ module CribbageGame
     THE_SHOW    = 40
     CRIB_SHOW   = 50
 
+    # Window Sizes
+
     WIDTH   = 800
     HEIGHT  = 600
 
     MID_X   = WIDTH / 2
     MID_Y   = HEIGHT / 2
+
+    # Colours
 
     BAIZE_COLOUR      = Gosu::Color.new( 0xff007000 )
     WATERMARK_COLOUR  = Gosu::Color.new( 0x20000000 )
@@ -23,28 +30,32 @@ module CribbageGame
     ARROW_COLOUR      = Gosu::Color.new( 0xe0ffcc00 )
     DISCARD_COLOUR    = Gosu::Color.new( 0xff104ec2 )
 
+    # Card Sizes
+
     CARD_HEIGHT = 150
     CARD_WIDTH  = 100
 
     CARD_GAP    = 12
 
-    COMPUTER_TOP  = CARD_GAP
+    #Positions
+
+    COMPUTER_TOP  = CARD_GAP    # Computer Hand
     COMPUTER_LEFT = CARD_GAP
 
-    PLAYER_TOP  = HEIGHT - (CARD_HEIGHT + CARD_GAP)
+    PLAYER_TOP  = HEIGHT - (CARD_HEIGHT + CARD_GAP)     # Player Hand
     PLAYER_LEFT = CARD_GAP
 
-    PACK_TOP  = MID_Y - (CARD_HEIGHT / 2)
+    PACK_TOP  = MID_Y - (CARD_HEIGHT / 2)               # Spare Pack
     PACK_LEFT = WIDTH - (CARD_WIDTH + CARD_GAP)
 
-    CRIB_TOP  = PACK_TOP
+    CRIB_TOP  = PACK_TOP                                # Crib
     CRIB_LEFT = PACK_LEFT - (CARD_WIDTH + CARD_GAP)
 
     BUTTON_HEIGHT = 40
-    DISCARD_TOP   = PLAYER_TOP - BUTTON_HEIGHT*2
+    DISCARD_TOP   = PLAYER_TOP - BUTTON_HEIGHT*2        # Discard Button
     DISCARD_LEFT  = CARD_GAP*3
 
-    SCORE_TOP  = CARD_GAP
+    SCORE_TOP  = CARD_GAP                               # Score Position
     SCORE_LEFT = WIDTH - CARD_WIDTH
   end
 end

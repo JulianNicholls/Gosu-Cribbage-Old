6167f85599aadb1f2bd6f9415238983f100d49cb
Julian Nicholls <juliannicholls29@gmail.com>
2013-12-17 22:06:08 +0000

    Added many more phases

--- constants.rb
+++ constants.rb
@@ -3,7 +3,10 @@ module CribbageGame
 
     # Phases
 
-    DEAL        = 0
+    INITIAL_CUT = 0
+    CPU_CUT     = 2
+    CUTS_MADE   = 4
+    DEAL        = 8
     DISCARDING  = 10
     CUT_CARD    = 20
     PLAY_31     = 30
@@ -27,7 +30,7 @@ module CribbageGame
     SCORE_TEXT_COLOUR = Gosu::Color.new( 0xffffcc00 )
     SCORE_NUM_COLOUR  = Gosu::Color.new( 0xffffff00 )
 
-    ARROW_COLOUR      = Gosu::Color.new( 0xe0ffcc00 )
+    ARROW_COLOUR      = Gosu::Color.new( 0xf0ffcc00 )
     DISCARD_COLOUR    = Gosu::Color.new( 0xff104ec2 )
 
     # Card Sizes
